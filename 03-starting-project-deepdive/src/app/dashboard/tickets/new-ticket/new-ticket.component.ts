08246e128688c5d3c11b5e9b3bb6e9a57581135e
Steffan Troxel <sjtroxel@alumni.iu.edu>
2025-04-04 15:32:50 -0500

    signal effects cleanup functions

--- 03-starting-project-deepdive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 03-starting-project-deepdive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
-export class NewTicketComponent {
+export class NewTicketComponent implements OnInit, AfterViewInit {
+ @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+// private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
 
+  ngOnInit() {
+    console.log('ON INIT');
+    console.log(this.form?.nativeElement);
+  }
+
+  ngAfterViewInit() {
+    console.log('AFTER VIEW INIT');
+    console.log(this.form?.nativeElement);
+  }
+
+  onSubmit(title: string, ticketText: string) {
+    console.log(title);
+    console.log(ticketText);
+    this.form?.nativeElement.reset();
+  }
 }
